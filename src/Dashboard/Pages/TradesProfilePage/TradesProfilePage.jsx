9776ba3a3032df6b1c9bc60634fed3c3eb6853e0
chamonsheikh121 <studentchamon1212@gmail.com>
2025-01-22 16:08:34 +0600

    Some documents added

---
+++ src/Dashboard/Pages/TradesProfilePage/TradesProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TradesProfilePage = () => {
+    return (
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, repellat.
+        </div>
+    );
+};
+
+export default TradesProfilePage;

bb071b521699dd443b0516dea8eb6dd1f51558a5
Ruhul <brainboostservices@gmail.com>
2025-02-08 11:37:17 +0600

    Update profile link logic and remove unused TradesDashboard import

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,7 +11,6 @@ import AskATrade from "../Pages/AskATrade/AskATrade";
 import ServiceSeeker from "../Pages/Registration/ServiceSeeker";
 import CustomerProfilePage from './../Dashboard/Pages/CustomerProfilePage/CustomerProfilePage';
 import CustomerDashboard from "../Dashboard/Pages/CustomerProfilePage/CustomerDashboard";
-import TradesDashboard from "../Dashboard/Pages/TradesProfilePage/TradesProfilePage/TradesDashboard";
 import TradesProfilePage from "../Dashboard/Pages/TradesProfilePage/TradesProfilePage";
 import AdminDashboard from "../Dashboard/Pages/AdminProfilePage/AdminDashboard/AdminDashboard";
 import AdminProfilePage from "../Dashboard/Pages/AdminProfilePage/AdminProfilePage/AdminProfilePage";
